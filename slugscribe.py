ed13d99342b7c93d8b77b1069b29082131eb5a2e
Joseph Howlett <JosephJHowlett@gmail.com>
2024-02-08 11:33:33 -0800

    working

--- slugscribe.py
+++ slugscribe.py
@@ -1,6 +1,7 @@
 from distutils.log import debug 
 from fileinput import filename 
 from flask import *  
+import whisper
 app = Flask(__name__)   
   
 @app.route('/')   
@@ -12,7 +13,16 @@ def success():
     if request.method == 'POST':   
         f = request.files['file'] 
         f.save(f.filename)   
-        return render_template("acknowledge.html", name = f.filename)   
+        return render_template(
+            "acknowledge.html",
+            name=f.filename,
+            text=transcribe_file(f.filename)
+        )
+
+def transcribe_file(fname):
+    model = whisper.load_model("base")
+    result = model.transcribe(fname, verbose=True)
+    return result["text"]
   
 if __name__ == '__main__':   
     app.run(debug=True)
