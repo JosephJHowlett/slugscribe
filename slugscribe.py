cafdaa057b3cd516e6d13f2b98e1a339b441a620
Joseph Howlett <JosephJHowlett@gmail.com>
2024-02-10 09:49:31 -0800

    printf

--- slugscribe.py
+++ slugscribe.py
@@ -2,6 +2,7 @@ from distutils.log import debug
 from fileinput import filename 
 from flask import *  
 from openai import OpenAI
+import os
 
 from rq import Queue
 from worker import conn
@@ -18,6 +19,8 @@ def success():
     if request.method == 'POST':   
         f = request.files['file'] 
         f.save(f.filename)   
+        print(os.path.exists(f.filename))
+        print('\n\n')
         text = q.enqueue(transcribe_file, f.filename)
         return render_template(
             "form.html",
